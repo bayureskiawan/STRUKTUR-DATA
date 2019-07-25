ad2ad53f77045be9dc81e39ecc76dde5dd24381e
bayureskiawan <52910212+bayureskiawan@users.noreply.github.com>
2019-07-25 12:18:14 +0700

    Add files via upload

---
+++ array.py
@@ -0,0 +1,12 @@
+x = [3, 30, 40]
+z = [30, 40, 70]
+
+# Menghitung Jumlah nilai semua array
+xTotal = sum(x)
+zTotal = sum(z)
+jumlah = xTotal + zTotal
+print("Jumlah Nilai Kedua Array:",jumlah)
+
+#Menggabungkan data kedua array
+print("Gabungan Array")
+print(x + z)
